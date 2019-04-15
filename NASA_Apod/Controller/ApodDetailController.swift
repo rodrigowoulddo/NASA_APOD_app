48f0b559d53e1c9380c67c6d22a96b9e4ab48175
Rodrigo <rodrigofgiglio@gmail.com>
2019-04-15 13:59:49 -0300

    Added assets and restructured architecture

---
+++ NASA_Apod/Controller/ApodDetailController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailController.swift
+//  NASA_Apod
+//
+//  Created by Rodrigo Giglio on 14/04/19.
+//  Copyright © 2019 Rodrigo Giglio. All rights reserved.
+//
+
+import UIKit
+
+class ApodDetailController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
