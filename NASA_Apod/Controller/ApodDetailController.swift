13fc863e0a5b4a025a6cffb5aac603ac81671c38
Rovane Moura <mourarovane@gmail.com>
2019-04-16 13:40:24 -0300

    some refinement to the detail screen

--- NASA_Apod/Controller/ApodDetailController.swift
+++ NASA_Apod/Controller/ApodDetailController.swift
@@ -26,18 +26,26 @@ class ApodDetailController: UIViewController {
             if let url = URL(string: apod.url) {
                 DispatchQueue.global().async {
                     if let data = try? Data(contentsOf:url) {
-                        DispatchQueue.main.async { self.pictureImageView.image = UIImage(data:data)
+                        DispatchQueue.main.async {
+                            self.pictureImageView.image = UIImage(data:data)
+                            self.pictureImageView.layer.borderColor = UIColor.white.cgColor
+                            self.pictureImageView.layer.borderWidth = 5
                             self.bgPictureImageView.image = UIImage(data:data)
                         }
-                        
                     }
                 }
             }
         }
         
         dateLabel.text = apod.date
+        
         titleLabel.text = apod.title
+        titleLabel.adjustsFontSizeToFitWidth = true
+        
         explanationLabel.text = apod.explanation
+        explanationLabel.numberOfLines = 0
+        explanationLabel.adjustsFontSizeToFitWidth = true
+        
         
     }
     
